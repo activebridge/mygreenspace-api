9a88932a154f6407eb7ef549b68341fa9192a4fb
Eugene Korpan <eugene@active-bridge.com>
2014-12-12 12:10:38 +0200

    Add missing attributes - user#email - user#picture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211181333) do
+ActiveRecord::Schema.define(version: 20141212100943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20141211181333) do
   create_table "users", force: true do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "first_name", null: false
-    t.string   "last_name",  null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
     t.string   "picture"
   end
