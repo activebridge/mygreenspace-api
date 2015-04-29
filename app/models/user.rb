7bbadcc320669bc57838927a7032ddacb2229e7e
Slav Kurilyak <kurilyak@gmail.com>
2015-04-29 15:50:48 -0700

    Add secure passwords. Add email and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :password,        presence: true,
+                              length: { minimum: 6 }, on: :create
+
+  validates :email,           uniqueness: { allow_nil: true },       on: :create
+  validates :email,           uniqueness: { case_sensitive: false }, on: :update
+
   validates :first_name,      presence: true
   validates :last_name,       presence: true
-  validates :email,           presence: true
-  validates :email,           uniqueness: true, if: :email
 
   has_many :spaces
   has_one :access_token, class_name: 'Doorkeeper::AccessToken', foreign_key: :resource_owner_id
